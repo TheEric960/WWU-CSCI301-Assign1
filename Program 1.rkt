bfbf0472089fafe5f5f1c4f04d1553bb91074e1e
Jack Wilson <wilso313@wwu.edu>
2019-05-01 21:36:21 -0700

    added inForm?

--- Program 1.rkt
+++ Program 1.rkt
@@ -9,7 +9,12 @@
   (lambda (n)
     (cond ((or (eq? 2 n) (eq? 3 n)) #t)
           ((or (< n 2) (eq? 0 (remainder n 2))) #f)
-          (else (let F ((i 2))
+          (else (let f ((i 2))
                   (cond ((>= i (/ n 2)) #t)
                         ((eq? (remainder n i) 0) #f)
                         (else (f (+ i 1)))))))))
+
+(define inForm?
+  (lambda (n)
+    (if (eq? 0 (remainder (- n 1) 4))
+        #t #f)))
