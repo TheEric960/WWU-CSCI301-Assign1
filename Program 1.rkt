b6d6098cd8756939baa084f11c67082539417f3b
Jack Wilson <wilso313@wwu.edu>
2019-05-01 17:46:48 -0700

    created prime? function

--- Program 1.rkt
+++ Program 1.rkt
@@ -1,10 +1,15 @@
 #lang R5RS
 
-(define getLen
-  (lambda (L) (cardinality 0 L)))
-
-(define cardinality
-  (lambda (n L)
+(define length
+  (lambda (L)
     (cond ((null? L) 0)
-          ((null? (cdr L)) (+ n 1))
-          (else (cardinality (+ n 1) (cdr L))))))
+          (else (+ 1 (length (cdr L)))))))
+
+(define prime?
+  (lambda (n)
+    (cond ((or (eq? 2 n) (eq? 3 n)) #t)
+          ((or (< n 2) (eq? 0 (remainder n 2))) #f)
+          (else (let F ((i 2))
+                  (cond ((>= i (/ n 2)) #t)
+                        ((eq? (remainder n i) 0) #f)
+                        (else (f (+ i 1)))))))))
